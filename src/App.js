8e06c47d6efabb480ca60bde7e6f2521e081a8c1
Neamul01 <neamulhaque2002@gmail.com>
2022-05-06 00:00:05 +0600

    components created and route set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Shared/Header/Header';
+import Footer from './components/Shared/Footer/Footer';
+import Home from './components/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <h2>Hello</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>All Right Reserve By &copy; Gadged Hub </h2>
+        </div>
+    );
+};
+
+export default Footer;
