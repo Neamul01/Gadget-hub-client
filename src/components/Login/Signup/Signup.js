fa9c09fe2f8ae906b1c6bb53ebb186c9d2ce729f
Neamul01 <neamulhaque2002@gmail.com>
2022-05-08 14:32:35 +0600

    toast added and error handleed in sign up and login page

--- src/components/Login/Signup/Signup.js
+++ src/components/Login/Signup/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useCreateUserWithEmailAndPassword, useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
@@ -8,11 +8,10 @@ import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Signup = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const [createUserWithEmailAndPassword, user, loading, error,] = useCreateUserWithEmailAndPassword(auth);
-    const [sendPasswordResetEmail, sending, resetError] = useSendPasswordResetEmail(
-        auth
-    );
+    const [sendPasswordResetEmail, sending, resetError] = useSendPasswordResetEmail(auth);
+    const [sendEmailVerification, varifySending, varifyError] = useSendEmailVerification(auth);
 
     const [gotEmail, setgotEmail] = useState('')
 
@@ -20,14 +19,19 @@ const Signup = () => {
     const location = useLocation();
     const from = location?.state?.from?.pathname || '/';
 
-    const onSubmit = (data) => {
+    const onSubmit = async (data) => {
         const name = data.name;
         const email = data.email;
         const pass = data.password;
         const confirmPass = data.confirmPassword;
 
         if (pass === confirmPass) {
-            createUserWithEmailAndPassword(email, pass)
+            await createUserWithEmailAndPassword(email, pass);
+            await sendEmailVerification();
+            toast.success('Varify Email send.\n Please check your Inbox or Spam..')
+        }
+        else {
+            toast.error("Email and Confirm Email dosn't match")
         }
     }
 
@@ -47,11 +51,11 @@ const Signup = () => {
         navigate(from, { replace: true })
     }
 
-    if (loading || sending) {
+    if (loading || sending || varifySending) {
         return <Loading></Loading>
     }
 
-    if (error || resetError) {
+    if (error || resetError || varifyError) {
         return toast.error(error.message)
     }
 
@@ -64,14 +68,16 @@ const Signup = () => {
             <form className="mt-6" id='signupForm' onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label htmlFor="name" className="block text-sm text-left text-gray-800 dark:text-gray-200">Name</label>
-                    <input type="text" {...register("name")}
+                    <input type="text" {...register("name", { required: true })}
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.name?.type === 'required' && <p className='text-red-400 text-left mb-2'><small>Name is required</small></p>}
                 </div>
 
                 <div>
                     <label htmlFor="email" className="block text-sm text-left text-gray-800 dark:text-gray-200">Email</label>
-                    <input onKeyUp={(e) => setgotEmail(e.target.value)} type="email" {...register("email")}
-                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" required />
+                    <input onKeyUp={(e) => setgotEmail(e.target.value)} type="email" {...register("email", { required: true })}
+                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.email?.type === 'required' && <p className='text-red-400 text-left mb-2'><small>Email is required</small></p>}
                 </div>
 
                 <div className="mt-4">
@@ -80,14 +86,16 @@ const Signup = () => {
                         <p onClick={handleResetPass} className="text-xs text-gray-600 dark:text-gray-400 hover:underline cursor-pointer">Forget Password?</p>
                     </div>
 
-                    <input type="password" {...register("password")}
-                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" required />
+                    <input type="password" {...register("password", { required: true })}
+                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.password?.type === 'required' && <p className='text-red-400 text-left mb-2'><small>Password is required</small></p>}
                 </div>
 
                 <div>
                     <label htmlFor="confirmPassword" className="block text-sm text-left text-gray-800 dark:text-gray-200">Confirm Password</label>
-                    <input type="password" {...register("confirmPassword")}
-                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" required />
+                    <input type="password" {...register("confirmPassword", { required: true })}
+                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.confirmPassword?.type === 'required' && <p className='text-red-400 text-left mb-2'><small>Confirm Password is required</small></p>}
                 </div>
 
                 <div className="mt-6">
